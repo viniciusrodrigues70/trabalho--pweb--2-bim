507149052207859c0ff792ab8a5a0234a1e907ac
Alefe2010 <amss34@aluno.ifal.edu.br>
2026-07-02 00:32:44 -0300

    Add files via upload

--- salas.js
+++ salas.js
@@ -30,17 +30,45 @@ app.get('/rooms', (req, res) => {
     return res.json(salas)
 });
 app.get('/rooms/:id', (req, res) => {
-    const { id } = req.params;
-    const sala = salas.find(sala => sala.id == id);
+    const {id} = req.params
+    const sala = salas.find(sala => sala.id === id)
 
     if (sala) {
-        return res.json(sala);
+        return res.json(sala)
     }
 
-    return res.status(404).json({
-        error: "Sala não encontrada com este ID!"
-    })
+    return res.status(404).send("A Sala com este ID não foi encontrada!")
+});
+app.put("/rooms/:id", (req, res) =>{
+    const {id} = req.params
+    const index = salas.findIndex(sala => sala.id == id);
+
+    if (index !== -1) {
+        const { name, capacidade, recursos, status } = req.body;
+        const salaAtualizada = {
+            id, 
+            name: name || salas[index].name,
+            //Propriedade capacidade: Se a resposta for sim (verdadeiro), ele transforma esse valor em número e salva na propriedade, se a resposta for não (falso/undefined), ele ignora o começo e pega o valor antigo que já estava guardado na array.
+            capacidade: capacidade ? Number(capacidade) : salas[index].capacidade,
+            recursos: recursos || salas[index].recursos,
+            status: status || salas[index].status
+        };
+
+        salas[index] = salaAtualizada
+        return res.status(200).send("Sala atualizada com sucesso!")
+    }
+    return res.status(404).send("A Sala com este ID não foi encontrada!")
+});
+app.delete("/rooms/:id", (req, res) => {
+    const {id}= req.params
+    const index = salas.findIndex(sala => sala.id == id);
+
+    if(index !== -1){
+        salas.splice(index, 1)
+        return res.status(200).send("Sala removida com sucesso!")
+    }
+    return res.status(404).send("A Sala com este ID não foi encontrada!")
 });
 app.listen(3000, () => {
-    console.log("Rodando em https://localhost3000")
+    console.log("Rodando em https://localhost:3000")
 });
